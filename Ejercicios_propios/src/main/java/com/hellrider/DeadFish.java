9d137800fa75d05e6ea89f6a881945eb05df1eae
JJGR90 <juanjoseguiradoruiz90@gmail.com>
2024-12-20 13:47:49 +0100

    kata

--- Ejercicios_propios/src/main/java/com/hellrider/DeadFish.java
+++ Ejercicios_propios/src/main/java/com/hellrider/DeadFish.java
@@ -1,8 +1,24 @@
 package com.hellrider;
 
-public class FindSecretMessage {
+/**
+ * Crea un analizador para interpretar y ejecutar el lenguaje Deadfish.
+ *
+ * Deadfish opera sobre un único valor en la memoria, que inicialmente está establecido en 0.
+ *
+ * Utiliza cuatro comandos de un solo carácter: i:
+ * Incrementar el valor d:
+ * Decrementar el valor s:
+ * Elevar al cuadrado el valor o:
+ *
+ * Imprime el valor en una matriz de resultados Todas las demás instrucciones no son operaciones y no tienen efecto.
+ *
+ * Ejemplos El programa "iiisdoso" debe devolver números [8, 64].
+ * El programa "iiisdosodddddiso" debe devolver números [8, 64, 3600].
+ * */
 
-    function findSecretMessage(paragraph) {
-        // what's the message?
+public class DeadFish {
+
+    public static int[] parse(String data) {
+        return new int[0];
     }
 }
--- Ejercicios_propios/src/test/java/Main/DeadFishTest.java
+++ Ejercicios_propios/src/test/java/Main/DeadFishTest.java
@@ -1,4 +1,13 @@
 package Main;
 
+import com.hellrider.DeadFish;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DeadFishTest {
+    @org.junit.jupiter.api.Test
+    public void exampleTests() {
+        assertArrayEquals(new int[] {8, 64}, DeadFish.parse("iiisdoso"));
+        assertArrayEquals(new int[] {8, 64, 3600}, DeadFish.parse("iiisdosodddddiso"));
+    }
 }
